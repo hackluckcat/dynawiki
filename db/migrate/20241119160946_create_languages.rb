42c98bdbd80f9e47a11cbbaf4a0ac4365b8d634e
KAWAKAMI Moeki <hi@moeki.org>
2024-11-20 01:16:56 +0900

    Add language model

---
+++ db/migrate/20241119160946_create_languages.rb
@@ -0,0 +1,10 @@
+class CreateLanguages < ActiveRecord::Migration[8.0]
+  def change
+    create_table :languages do |t|
+      t.string :name, null: false, index: { unique: true }
+      t.string :code, null: false, index: { unique: true }
+
+      t.timestamps
+    end
+  end
+end
