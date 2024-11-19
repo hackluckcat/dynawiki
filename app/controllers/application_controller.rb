7c89336362476d8f4e9fc4334eaa262e4255d217
KAWAKAMI Moeki <hi@moeki.org>
2024-11-20 07:51:23 +0900

    Set locale when access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_locale
+
+  helper_method :language
+
+  def language
+    @language ||= Language.find_by(code: params[:code])
+  end
+
+  private def set_locale
+    I18n.locale = language.code
+  end
 end
