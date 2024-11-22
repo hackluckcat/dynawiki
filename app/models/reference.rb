f26d862b53dbce11f185e6439db01d9676c6d9a3
KAWAKAMI Moeki <hi@moeki.org>
2024-11-22 19:34:23 +0900

    Add .from_html method

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -1,3 +1,17 @@
 class Reference < ApplicationRecord
   belongs_to :page
+
+  def self.from_html(html_content:, url:)
+    doc = Nokogiri::HTML(html_content)
+    doc.search("script, style, noscript").remove
+    body = doc.text.strip
+    body = body.gsub(/\s+/, " ")
+    body = body.gsub(/\n\s*\n/, "\n")
+    title = doc.at("title").text
+    uri = URI.parse(url)
+    base_url = "#{uri.scheme}://#{uri.host}"
+    icon = doc.at('[rel="icon"]')
+    image_url = icon ? "#{base_url}#{icon["href"]}" : ""
+    new(title:, body:, url:, base_url:, image_url:)
+  end
 end
--- test/models/reference_test.rb
+++ test/models/reference_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class ReferenceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test ".from_html" do
+    html_content = <<~HTML
+      <html>
+        <head>
+          <title>Example</title>
+          <link rel="icon" href="/icon.png" />
+        </head>
+        <body>
+          This is example page.
+        </body>
+      </html>
+    HTML
+
+    reference = Reference.from_html(html_content:, url: "https://example.com/example")
+    assert_equal "https://example.com/example", reference.url
+    assert_equal "https://example.com", reference.base_url
+    assert_equal "Example", reference.title
+    assert_equal "https://example.com/icon.png", reference.image_url
+    assert_equal "Example This is example page.", reference.body
+  end
 end
